c357c29b489c35eed6bdf54674c19b341a24750e
Eduard Hovhannisyan <edhovhannisyan04@gmail.com>
2026-04-01 01:01:45 -0700

    changes

--- src/deals.js
+++ src/deals.js
@@ -9,7 +9,7 @@ const deals = [
   },
   {
     vehicle: "2026 Toyota Camry LE",
-    img: "vehiclePhotos/toyota_camry_2026_le_white.png",
+    img: "vehiclePhotos/toyota_camry_le_2026_white.png",
     payment: "$289",
     months: "36",
     disclosure:
@@ -19,6 +19,14 @@ const deals = [
     vehicle: "2026 Toyota bZ XLE XLE FWD PLUS",
     img: "vehiclePhotos/toyota_bz_xle_fwd_2026_blue.avif",
     payment: "$309",
+    months: "36",
+    disclosure:
+      "Terms available on approved credit through Toyota Financial Services (TFS) at participating Toyota dealers. Not all customers qualify.",
+  },
+  {
+    vehicle: " 2026 Corolla 2WD 4Dr. Sedan SE L4 CVT-F",
+    img: "vehiclePhotos/toyota_corolla_se_2026_blue.jpg",
+    payment: "$259",
     months: "36",
     disclosure:
       "Terms available on approved credit through Toyota Financial Services (TFS) at participating Toyota dealers. Not all customers qualify.",
