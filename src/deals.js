8990e9b1ccff59dde24e07e00e63843b0257d27e
Eduard Hovhannisyan <edhovhannisyan04@gmail.com>
2026-04-01 00:22:25 -0700

    initial commit

---
+++ src/deals.js
@@ -0,0 +1,31 @@
+const deals = [
+  {
+    id: 1,
+    img: "vehiclePhotos/toyota_camry_2026_le_white.png",
+    vehicle: "2026 Toyota Camry LE",
+    payment: "$359.99",
+    months: "36",
+    disclosure:
+      "Terms available on approved credit through Toyota Financial Services (TFS) at participating Toyota dealers. Not all customers qualify.",
+  },
+  {
+    id: 2,
+    vehicle: "2026 Toyota bZ XLE XLE FWD PLUS",
+    img: "vehiclePhotos/toyota_camry_2026_le_white.png",
+    payment: "$400.22",
+    months: "36",
+    disclosure:
+      "Terms available on approved credit through Toyota Financial Services (TFS) at participating Toyota dealers. Not all customers qualify.",
+  },
+  {
+    id: 3,
+    vehicle: "2026 Toyota Grand Highlander Hybrid Limited LIMITED HYBRID AWD",
+    img: "vehiclePhotos/toyota_camry_2026_le_white.png",
+    payment: "$259.99",
+    months: "36",
+    disclosure:
+      "Terms available on approved credit through Toyota Financial Services (TFS) at participating Toyota dealers. Not all customers qualify.",
+  },
+];
+
+export default deals;
