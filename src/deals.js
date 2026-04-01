511c0b3ea5b770e14b3a143cc0cc0ace6f0460d0
Eduard Hovhannisyan <edhovhannisyan04@gmail.com>
2026-04-01 00:52:27 -0700

    adde new specials

--- src/deals.js
+++ src/deals.js
@@ -1,27 +1,24 @@
 const deals = [
   {
-    id: 1,
-    img: "vehiclePhotos/toyota_camry_2026_le_white.png",
+    vehicle: "2026 Toyota Tacoma SR5 4X2 DOUBLE CAB",
+    img: "vehiclePhotos/toyota_tacoma_sr5_4x2_doublacab_2026_blue.png",
+    payment: "$279",
+    months: "36",
+    disclosure:
+      "Terms available on approved credit through Toyota Financial Services (TFS) at participating Toyota dealers. Not all customers qualify.",
+  },
+  {
     vehicle: "2026 Toyota Camry LE",
-    payment: "$359.99",
+    img: "vehiclePhotos/toyota_camry_2026_le_white.png",
+    payment: "$289",
     months: "36",
     disclosure:
       "Terms available on approved credit through Toyota Financial Services (TFS) at participating Toyota dealers. Not all customers qualify.",
   },
   {
-    id: 2,
     vehicle: "2026 Toyota bZ XLE XLE FWD PLUS",
-    img: "vehiclePhotos/toyota_camry_2026_le_white.png",
-    payment: "$400.22",
-    months: "36",
-    disclosure:
-      "Terms available on approved credit through Toyota Financial Services (TFS) at participating Toyota dealers. Not all customers qualify.",
-  },
-  {
-    id: 3,
-    vehicle: "2026 Toyota Grand Highlander Hybrid Limited LIMITED HYBRID AWD",
-    img: "vehiclePhotos/toyota_camry_2026_le_white.png",
-    payment: "$259.99",
+    img: "vehiclePhotos/toyota_bz_xle_fwd_2026_blue.png",
+    payment: "$309",
     months: "36",
     disclosure:
       "Terms available on approved credit through Toyota Financial Services (TFS) at participating Toyota dealers. Not all customers qualify.",
