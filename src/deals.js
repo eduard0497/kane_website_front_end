7a76a2d219c5b2a3bb96a8fb678535ca000cde54
Eduard Hovhannisyan <edhovhannisyan04@gmail.com>
2026-04-01 01:03:30 -0700

    changes

--- src/deals.js
+++ src/deals.js
@@ -1,6 +1,6 @@
 const deals = [
   {
-    vehicle: "2026 Toyota Tacoma SR5 4X2 DOUBLE CAB",
+    vehicle: "2026 Toyota Tacoma SR5 DOUBLE CAB",
     img: "vehiclePhotos/toyota_tacoma_sr5_4x2_doublacab_2026_blue.png",
     payment: "$279",
     months: "36",
@@ -16,7 +16,7 @@ const deals = [
       "Terms available on approved credit through Toyota Financial Services (TFS) at participating Toyota dealers. Not all customers qualify.",
   },
   {
-    vehicle: "2026 Toyota bZ XLE XLE FWD PLUS",
+    vehicle: "2026 Toyota bZ XLE FWD PLUS",
     img: "vehiclePhotos/toyota_bz_xle_fwd_2026_blue.avif",
     payment: "$309",
     months: "36",
@@ -24,7 +24,7 @@ const deals = [
       "Terms available on approved credit through Toyota Financial Services (TFS) at participating Toyota dealers. Not all customers qualify.",
   },
   {
-    vehicle: " 2026 Corolla 2WD 4Dr. Sedan SE L4 CVT-F",
+    vehicle: " 2026 Corolla 2WD SE",
     img: "vehiclePhotos/toyota_corolla_se_2026_blue.jpg",
     payment: "$259",
     months: "36",
