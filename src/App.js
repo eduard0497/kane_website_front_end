1d9cf5ef260af037873c28010a2c6994db5b9e83
Eduard Hovhannisyan <edhovhannisyan04@gmail.com>
2026-04-05 18:42:40 -0700

    deal updates

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./Home";
-// import Admin from "./Admin";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      {/* <Route path="/admin" element={<Admin />} /> */}
     </Routes>
   );
 }
